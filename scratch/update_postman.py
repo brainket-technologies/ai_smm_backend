97ea1c555bdcf31e88637155dfdba0d8d618f082
Firoz Mohammad <firozm613@gmail.com>
2026-05-02 03:12:06 +0530

    docs: parameterize device headers in postman collection

--- scratch/update_postman.py
+++ scratch/update_postman.py
@@ -10,20 +10,28 @@ def process_item(item):
     
     if 'request' in item:
         headers = item['request'].get('header', [])
-        has_apikey = any(h.get('key') == 'apikey' for h in headers)
-        has_device_id = any(h.get('key') == 'device-id' for h in headers)
-        has_device_type = any(h.get('key') == 'device-type' for h in headers)
-        
-        if has_apikey:
-            if not has_device_id:
-                headers.append({ "key": "device-id", "value": "unique_device_id_123", "type": "text" })
-            if not has_device_type:
-                headers.append({ "key": "device-type", "value": "android", "type": "text" })
+        for h in headers:
+            if h.get('key') == 'device-id':
+                h['value'] = "{{device_id}}"
+            elif h.get('key') == 'device-type':
+                h['value'] = "{{device_type}}"
         
         item['request']['header'] = headers
 
 for item in collection['item']:
     process_item(item)
+
+# Add variables to the variable list if not already there
+if 'variable' not in collection:
+    collection['variable'] = []
+
+variables = collection['variable']
+var_keys = [v.get('key') for v in variables]
+
+if 'device_id' not in var_keys:
+    variables.append({ "key": "device_id", "value": "unique_device_id_123" })
+if 'device_type' not in var_keys:
+    variables.append({ "key": "device_type", "value": "android" })
 
 with open('postman_collection.json', 'w') as f:
     json.dump(collection, f, indent=4)
